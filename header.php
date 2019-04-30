a9b5dce4f774dd63620f2365d2b5ce6462945576
Jessica Butler <jessie.butler89@gmail.com>
2019-04-30 18:46:05 +0100

    push project to git
    backing up php project using xaamp and no libraries on git

---
+++ header.php
@@ -0,0 +1,24 @@
+<DOCTYPE! html>
+<head>
+    <style>
+        body {
+            background-image: url("images/sealgray%20-%20Copy.png");
+            background-repeat: repeat;
+        }
+    </style>
+</head>
+<body>
+    <?php
+      echo "<header class=\"header\" id=\"header\">
+                <img src=\"images/modelbanner.jpg\" alt=\"Banner\" width=\"100%\" height=\"10%\">
+                <nav class=\"nav\">
+                    <ul>
+                        <li class=\"Index\"><a href=\"index.php\">Index</a></li>
+                        <li class=\"Offices\"><a href=\"offices.php\">Offices</a>
+                        <li class=\"Payments\"><a href=\"payments.php\">Payments</a></li>
+                    </ul>
+                </nav>
+            </header>";
+      ?>
+</body>
+</DOCTYPE!>
